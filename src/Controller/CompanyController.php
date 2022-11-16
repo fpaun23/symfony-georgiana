05869d291f23348bda17fd8f1efbb17178aeb5b2
Georgiana Panaete <96georgiana.panaete@gmail.com>
2022-11-16 16:00:18 +0200

    task 1

---
+++ src/Controller/CompanyController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CompanyController extends AbstractController
+{
+    #[Route('/company', name: 'app_company')]
+    public function index(): Response
+    {
+        return $this->render('company/index.html.twig', [
+            'controller_name' => 'CompanyPhpController',
+        ]);
+    }
+}
