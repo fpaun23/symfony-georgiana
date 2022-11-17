db68bd2dcc85b1df800bdf30c7b4dec921e5dcb9
Georgiana Panaete <96georgiana.panaete@gmail.com>
2022-11-17 11:06:52 +0200

    remove unused class

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,8 +6,6 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-
 
 class HomeController extends AbstractController
 {
