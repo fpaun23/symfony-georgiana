8da3e66db3b12e6b33593414dc99be0e7a0aba4b
Georgiana Panaete <96georgiana.panaete@gmail.com>
2022-11-20 21:25:50 +0200

    task_3

---
+++ src/Constants.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+class Constants
+{
+    //add array constant
+    public const RANGE = [2,12,10,50];
+}
+
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,15 +6,45 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Constants;
 
 class ContactController extends AbstractController
 {
+    public $name = 'Georgiana Panaete';
+
     public function displayContact(): Response
     {
-        $name = 'Georgiana Panaete';
+        return $this->render(
+            'contact/index.html.twig', [
+            'name' => $this->name,
+            ]
+        );
+    }
 
-        return $this->render('contact/index.html.twig', [
-            'name' => $name,
-        ]);
+    public function addContact(): Response
+    {
+        $errors = [];
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $name = $_POST["name"];
+            $email = $_POST["email"];
+            $description = $_POST["description"];
+        
+            if ((strlen($name) < Constants::RANGE[0] || strlen($name) > Constants::RANGE[1]) || !preg_match("/^[a-zA-Z]+$/", $name)) {
+                $errors[] = "name";
+            }
+
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $errors[] = "email";
+            }
+
+            if (strlen($description) < Constants::RANGE[2] || strlen($description) > Constants::RANGE[3]) {
+                $errors[] = "description";
+            }
+        }
+            return $this->render(
+                'contact/add/index.html.twig', [
+                'errors' => $errors,
+                ]
+            );
     }
 }
