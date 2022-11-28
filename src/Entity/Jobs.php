7f1110d648a3ca573a23d41e5a9fa635f3c8a34d
Georgiana Panaete <96georgiana.panaete@gmail.com>
2022-11-28 12:57:04 +0200

    task5

---
+++ src/Entity/Jobs.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\JobsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: JobsRepository::class)]
+class Jobs
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $jobs = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getJobs(): ?string
+    {
+        return $this->jobs;
+    }
+
+    public function setJobs(string $jobs): self
+    {
+        $this->jobs = $jobs;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
