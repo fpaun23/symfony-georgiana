c3307c1ecf91e44dabfb4de01fd92b3431709bdf
Georgiana Panaete <96georgiana.panaete@gmail.com>
2022-12-06 16:24:04 +0200

    TASK 6

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Company;
-use Psr\Log\LoggerInterface;
+use App\Repository\CompanyRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
@@ -19,13 +17,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
  */
 class CompanyController extends AbstractController
 {
-    /**
-     * logger
-     *
-     * @var mixed
-     */
-    private $logger;
-
     /**
      * companyName
      *
@@ -38,7 +29,7 @@ class CompanyController extends AbstractController
      */
     private ManagerRegistry $doctrine;
 
-    private mixed $repository;
+    private CompanyRepository $companyRepository;
 
     /**
      * __construct
@@ -46,11 +37,10 @@ class CompanyController extends AbstractController
      * @param  mixed $logger
      * @return void
      */
-    public function __construct(ManagerRegistry $doctrine, LoggerInterface $logger)
+    public function __construct(ManagerRegistry $doctrine, CompanyRepository $companyRepository)
     {
         $this->doctrine = $doctrine;
-        $this->logger = $logger;
-        $this->repository = $this->doctrine->getRepository(Company::class);
+        $this->companyRepository = $companyRepository;
     }
 
     /**
@@ -76,37 +66,21 @@ class CompanyController extends AbstractController
      */
     public function addCompany(Request $request): Response
     {
-        $form = $this->createFormBuilder()
-            ->add('name', TextType::class)
-            ->add('save', SubmitType::class, ['label' => 'Add'])
-            ->getForm();
+        //get all request parameters : $request->query->all()
 
-        $form->handleRequest($request);
+        //get request parameter with key 'name
+        $name = $request->get('name');
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        //create new Company objec
+        $newCompany = new Company();
+        $newCompany->setName($name);
 
-            $data = $form->getData();
-            $name = $data['name'];
-            $entityManager = $this->doctrine->getManager();
-            $company = new Company();
-            $company->setName($name);
-            $entityManager->persist($company);
-            $entityManager->flush();
+        //call the Repository save function with flush set to true
+        $companySaved = $this->companyRepository->save($newCompany);
 
-            return new Response(
-                sprintf(
-                    'Id %d and name %s',
-                    $company->getId(),
-                    $company->getName()
-                )
-            );
-        }
-
-        return $this->render(
-            'company/index.html.twig',
+        return new JsonResponse(
             [
-                'form' => $form->createView(),
-                'company_name' => $this->companyName,
+                'saved' => $companySaved,
             ]
         );
     }
@@ -118,21 +92,14 @@ class CompanyController extends AbstractController
      * @param  mixed $id
      * @return Response
      */
-    public function updateCompany(int $id): Response
+    public function updateCompany(Request $request, int $id): Response
     {
-        $updateId = null;
-        $entityManager = $this->doctrine->getManager();
-        $company = $entityManager->getRepository(Company::class)->find($id);
+        $requestParams = $request->query->all();
+        $updateResult = $this->companyRepository->update($id, $requestParams);
 
-        if (!empty($company)) {
-            $updateId = $company->getId();
-            $company->setName('Georgiana12');
-            $entityManager->flush();
-        }
         return new JsonResponse(
             [
-            'update' => !empty($updateId),
-            'updateId' => $updateId
+                'rows_updated' => $updateResult
             ]
         );
     }
@@ -146,20 +113,12 @@ class CompanyController extends AbstractController
      */
     public function deleteCompany(int $id): Response
     {
-        $deletedId = null;
-        $entityManager = $this->doctrine->getManager();
-        $company = $entityManager->getRepository(Company::class)->find($id);
-
-        if (!empty($company)) {
-            $deletedId = $company->getId();
-            $entityManager->remove($company);
-            $entityManager->flush();
-        }
+        $companyToDelete = $this->companyRepository->find($id);
+        $companyDeleted = $this->companyRepository->remove($companyToDelete);
 
         return new JsonResponse(
             [
-            'detleted' => !empty($deletedId),
-            'deleteId' => $deletedId
+                'rows_deleted' => $companyDeleted
             ]
         );
     }
@@ -176,12 +135,12 @@ class CompanyController extends AbstractController
         $query = $queryBuilder
             ->select('c')
             ->from(Company::class, 'c')
-            ->getQuery();
+            ->getQuery()
+            ->getArrayResult();
 
-        $sql = $query->getArrayResult();
-        return new JsonResponse($sql);
+        return new JsonResponse($query);
     }
-    
+
     /**
      * companyId
      *
@@ -197,11 +156,12 @@ class CompanyController extends AbstractController
             ->from(Company::class, 'c')
             ->where('c.id = :identifier')
             ->setParameter('identifier', $id)
-            ->getQuery();
-        $sql = $query->getResult();
-        return new JsonResponse($sql);
+            ->getQuery()
+            ->getResult();
+
+        return new JsonResponse($query);
     }
-    
+
     /**
      * companyName
      *
@@ -217,11 +177,12 @@ class CompanyController extends AbstractController
             ->from(Company::class, 'c')
             ->where('c.name = :identifier')
             ->setParameter('identifier', $name)
-            ->getQuery();
-        $sql = $query->getResult();
-        return new JsonResponse($sql);
+            ->getQuery()
+            ->getResult();
+
+        return new JsonResponse($query);
     }
-        
+
     /**
      * likeCompanyName
      *
@@ -237,8 +198,8 @@ class CompanyController extends AbstractController
             ->from(Company::class, 'c')
             ->where('c.name LIKE :identifier')
             ->setParameter('identifier', '%' . $name . '%')
-            ->getQuery();
-        $sql = $query->getResult();
-        return new JsonResponse($sql);
+            ->getQuery()
+            ->getResult();
+        return new JsonResponse($query);
     }
 }
--- src/Repository/CompanyRepository.php
+++ src/Repository/CompanyRepository.php
@@ -20,23 +20,53 @@ class CompanyRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Company::class);
     }
-
-    public function save(Company $entity, bool $flush = false): void
+    
+    /**
+     * save a new company
+     *
+     * @param  mixed $entity an object of thpe EntityCompany
+     * @return bool
+     */
+    public function save(Company $entity): bool
     {
         $this->getEntityManager()->persist($entity);
+        $this->getEntityManager()->flush();
 
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
+        //if previous flush method is succesfull the $entity->getId() will return the id of the inserted company
+        //if previous flush method is not succesfull $entity->getId() will return NULL
+        return $entity->getId() > 0; // if null willreturn false
     }
 
-    public function remove(Company $entity, bool $flush = false): void
+    public function remove(Company $entity): void
     {
         $this->getEntityManager()->remove($entity);
+        $this->getEntityManager()->flush();
+    }
 
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
+    /**
+     * do an update on the company table
+     *
+     * @param  mixed $id the id of the company record from the database to be updated
+     * @param  mixed $params a list of params with the fields and values to update
+     * @return int
+     */
+    public function update(int $id, array $params): int
+    {
+        //create a new query builder object
+        $queryBuilder = $this->createQueryBuilder('c');
+
+        //prepare an update statement
+        $nbUpdatedRows = $queryBuilder->update()
+            ->set('c.name', ':companyName')
+            ->where('c.id = :companyId')
+            ->setParameter('companyName', $params['name'])
+            ->setParameter('companyId', $id)
+            ->getQuery()
+            ->execute();
+            //->execute() will return the number of affected rows by the sql query (in this case updated rows)
+            //the result is 0 if no updates and >= 1 if updates done
+
+            return $nbUpdatedRows;
     }
 
 //    /**
