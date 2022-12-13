23a981bdd967a1169ea2defbff3fe7e925b41b97
Georgiana Panaete <96georgiana.panaete@gmail.com>
2022-12-13 03:06:47 +0200

    task 8 WIP

--- src/Entity/Jobs.php
+++ src/Entity/Jobs.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\JobsRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: JobsRepository::class)]
@@ -13,15 +14,40 @@ class Jobs
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\ManyToOne(inversedBy: 'jobs')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Company $company = null;
+
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 500)]
     private ?string $description = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $createdAt = null;
+
+    #[ORM\Column]
+    private ?int $active = null;
+
+    #[ORM\Column]
+    private ?int $priority = null;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getCompany(): ?Company
+    {
+        return $this->company;
+    }
+
+    public function setCompany(?Company $company): self
+    {
+        $this->company = $company;
+
+        return $this;
     }
 
     public function getName(): ?string
@@ -44,6 +70,42 @@ class Jobs
     public function setDescription(string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getActive(): ?int
+    {
+        return $this->active;
+    }
+
+    public function setActive(int $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    public function getPriority(): ?int
+    {
+        return $this->priority;
+    }
+
+    public function setPriority(int $priority): self
+    {
+        $this->priority = $priority;
 
         return $this;
     }
--- src/Repository/JobsRepository.php
+++ src/Repository/JobsRepository.php
@@ -21,23 +21,64 @@ class JobsRepository extends ServiceEntityRepository
         parent::__construct($registry, Jobs::class);
     }
 
-    public function save(Jobs $entity, bool $flush = false): void
+    /**
+     * save
+     *
+     * @param  mixed $entity
+     * @return bool
+     */
+    public function save(Jobs $entity): bool
     {
         $this->getEntityManager()->persist($entity);
+        $this->getEntityManager()->flush();
 
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
+        //if previous flush method is succesfull the $entity->getId() will return the id of the inserted company
+        //if previous flush method is not succesfull $entity->getId() will return NULL
+        return $entity->getId() > 0; // if null willreturn false
     }
-
-    public function remove(Jobs $entity, bool $flush = false): void
+          
+    /**
+     * remove
+     *
+     * @param  mixed $entity
+     * @return void
+     */
+    public function remove(Jobs $entity): void
     {
         $this->getEntityManager()->remove($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
+        $this->getEntityManager()->flush();
     }
+
+    /**
+     * do an update on the company table
+     *
+     * @param  mixed $id     the id of the company record from the database to be updated
+     * @param  mixed $params a list of params with the fields and values to update
+     * @return int
+     */
+    public function update(int $id, array $params): int
+    {
+        //create a new query builder object
+        $queryBuilder = $this->createQueryBuilder('j');
+
+        //prepare an update statement
+        $nbUpdatedRows = $queryBuilder->update()
+            ->set('j.name', ':jobName')
+            ->set('j.description', ':jobDescription')
+            ->set('j.active', ':jobActive')
+            ->set('j.priority', ':jobPriority')
+            ->where('j.id = :jobId')
+            ->setParameter('jobName', $params['name'])
+            ->setParameter('jobDescription', $params['description'])
+            ->setParameter('jobActive', $params['active'])
+            ->setParameter('jobPriority', $params['priority'])
+            ->setParameter('jobId', $id)
+            ->getQuery()
+            ->execute();
+
+        return $nbUpdatedRows;
+    }
+
 
 //    /**
 //     * @return Jobs[] Returns an array of Jobs objects
